cbbfadd0cc05d8481338c8fc15d809de7dbba3d3
Storm Visser <storm.visser@student.nhlstenden.com>
2021-06-15 14:46:12 +0200

    inital commit

---
+++ Code/DatabasesSnelheidTesten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabasesSnelheidTesten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
