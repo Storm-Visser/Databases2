1d44080fa0837be28f7853afe5c3f23a0307a09a
Storm Visser <storm.visser@student.nhlstenden.com>
2021-06-22 23:47:12 +0200

    TODO

--- Code/DatabasesSnelheidTesten/Program.cs
+++ Code/DatabasesSnelheidTesten/Program.cs
@@ -79,6 +79,7 @@ namespace DatabasesSnelheidTesten
             Console.WriteLine("-----------------------------------------");
             Console.WriteLine("1,000,000 rows");
             Console.WriteLine("Time: " + adonetTest.testDelete(1000) + "ms");
+            //TODO-----------------------Create seperate method to reset the seed
             Console.WriteLine("End of test; ADO.NET Delete");
             Console.WriteLine("-------------------------------------------------------------------");
         }
